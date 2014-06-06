b023c08e1d40778a6466388ffdeb0ac12d1b1526
djohnson0610 <djohnson0610@hotmail.com>
2014-06-05 23:27:20 -0700

    Simple validations

--- app/models/twit.rb
+++ app/models/twit.rb
@@ -1,3 +1,6 @@
 class Twit < ActiveRecord::Base
 	belongs_to :user
-end
+	validates :content, length: 1..140
+  end
+
+  #error handling at twit creation
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_many :twits
+  validates :name, presence: true
+  validates :username, :email, uniqueness: true
+  validates :password, length: 6..15
 end
+
+#error handling at account creation/update
