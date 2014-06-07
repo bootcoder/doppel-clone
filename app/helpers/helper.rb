28e1a4a883f2b29c29891b15219c3eb872bf3375
djohnson0610 <djohnson0610@hotmail.com>
2014-06-06 17:02:07 -0700

    follow button

--- app/helpers/helper.rb
+++ app/helpers/helper.rb
@@ -4,4 +4,22 @@ helpers do
       @current_user ||= User.find_by_id(session[:id])
     end
   end
+
+  def is_following?(user_id)
+  	list = User.find(user_id).follower_users
+  	list.each { |user| return true if user.id == current_user.id }
+  	false
+  end
+
+  def create_follow(user_id)
+  	current_user
+		FollowConnection.create( follower_user_id: user_id, followed_user_id: @current_user.id)
+  end
+
+  def unfollow(user_id)
+  	current_user
+  	FollowConnection.destroy( follower_user_id: user_id, followed_user_id: @current_user.id)
+  end
+
 end
+
